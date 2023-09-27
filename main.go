75e2facd4874d5b0ceed3276e522c299455bb957
mavro <mrojasb2000@gmail.com>
2023-09-26 21:49:56 -0300

    chore: add basic routes with Gin

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"name":    "Mavro",
+			"message": "Welcome to Gin Framewor",
+		})
+	})
+
+	router.Run()
 }
