d1eee564b659d38a6d1f09fb8e54ceb3c1a06136
mavro <mrojasb2000@gmail.com>
2023-09-28 19:28:23 -0300

    feat: add basic routes and handler

--- main.go
+++ main.go
@@ -9,12 +9,23 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name":    "Mavro",
-			"message": "Welcome to Gin Framewor",
-		})
-	})
+	router.GET("/", rootHandler)
+
+	router.GET("/hello", helloHandler)
 
 	router.Run()
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name":    "Mavro",
+		"message": "Welcome to Gin Framewor",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"content": "Hello World",
+		"bio":     "Golang Microservices",
+	})
 }
